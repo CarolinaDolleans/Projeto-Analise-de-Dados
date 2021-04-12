bbdd2cc843da21850ace7ab83105d269bb6fa6be
Carolina Dolléans <carolinacabral@hotmail.fr>
2021-04-12 19:38:55 -0300

    Exercício Objetos e Funções.

---
+++ objetos-e-funcoes.R
@@ -0,0 +1,23 @@
+# Banco iris
+
+banco_iris <- iris
+
+
+# Pacote dplyr
+
+library(dplyr)
+
+
+# Banco com apenas duas variáveis
+
+banco_reduzido <- banco_iris %>%
+  select(Sepal.Length, Petal.Length)
+
+
+# Regressão linear entre Petal.Length (VD) e Sepal.Length (VI)
+
+reg_iris <- lm(Petal.Length ~ Sepal.Length, banco_reduzido)
+
+# Sumário da regressão
+
+summary(reg_iris)
