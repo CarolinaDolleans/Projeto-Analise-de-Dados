93c2f2e62668507b886ccf64da6e24b19d1630ec
Carolina Dolléans <carolinacabral@hotmail.fr>
2021-04-15 15:56:10 -0300

    Atualização

--- tipos_de_objetos.R
+++ tipos_de_objetos.R
@@ -1,28 +1,41 @@
-# Intalaçao do pacote epptools
+# Intalação do pacote epptools
+
 install.packages('eeptools')
 
-# Chamamos pacote
+
+# Chamamos pacotes
+
 library(ggplot2)
+
 library(eeptools)
+
 
 # Vetor das cidades
 
 municipios <- c("Afrânio", "Bezerros", "Capoeiras", "Garanhuns", "Recife")
 
-# Vetor com a populaçao por cidade
+
+# Vetor com a população por cidade
 
 populaçao_2020 <- c("19810", "60880", "20048", "140577", "1555000" )
+
 
 # Vetor com a letalidade do covid por cidade
 
 letalidade_covid <-c("1,56%", "2,93%", "1,70%", "1,87%", "3,69%")
 
-# Criaçao do dataframe
+
+# Criação do dataframe
 
 municipios_pe_covid <- data.frame(
   municipio = municipios,
   populaçao = populaçao_2020,
   letalidade = letalidade_covid
 )
+
+# Verificação da estrutura do objeto
+
+str(municipios_pe_covid)
+
 
 
